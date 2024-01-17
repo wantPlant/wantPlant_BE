aa60f01daaebc58b9b0ec497c293667c2d202a91
donghyun1208 <ilovekdh1208@ajou.ac.kr>
2024-01-17 22:54:04 +0900

    docs: init project

---
+++ wantPlant/src/main/java/umc/wantPlant/WantPlantApplication.java
@@ -0,0 +1,13 @@
+package umc.wantPlant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantPlantApplication.class, args);
+	}
+
+}
