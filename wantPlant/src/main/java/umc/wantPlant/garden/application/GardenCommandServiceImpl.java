0069ba888446e708b51743b800b5d8dd7690b700
이승준 <sengjun0624@naver.com>
2024-01-20 00:13:51 +0900

    :sparkles: FEAT : Implement creatGardenn funtion

--- wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandServiceImpl.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandServiceImpl.java
@@ -6,14 +6,39 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import umc.wantPlant.garden.domain.Garden;
 import umc.wantPlant.garden.domain.dto.GardenRequestDTO;
+import umc.wantPlant.garden.domain.enums.GardenCategories;
+import umc.wantPlant.garden.repository.GardenRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GardenCommandServiceImpl implements GardenCommandService {
+
+	private final GardenRepository gardenRepository;
+
 	@Override
 	@Transactional
-	public Garden creatGarden(GardenRequestDTO.CreatDTO creat) {
-		return null;
+	public Garden creatGarden(GardenRequestDTO.GardenCreatDTO creat) {
+
+		GardenCategories gardenCategories = null;
+
+		switch (creat.getGardenCategory()) {
+			case 1:
+				gardenCategories = GardenCategories.EXERCISE;
+				break;
+			case 2:
+				gardenCategories = GardenCategories.STUDY;
+				break;
+			case 3:
+				gardenCategories = GardenCategories.HOBBY;
+				break;
+		}
+
+		return gardenRepository.save(
+			Garden.builder()
+				.name(creat.getName())
+				.description(creat.getDescription())
+				.category(gardenCategories)
+				.build());
 	}
 }
--- wantPlant/src/main/java/umc/wantPlant/garden/ui/GardenController.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/ui/GardenController.java
@@ -1,4 +1,46 @@
 package umc.wantPlant.garden.ui;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import umc.wantPlant.apipayload.ApiResponse;
+import umc.wantPlant.garden.application.GardenCommandService;
+import umc.wantPlant.garden.domain.Garden;
+import umc.wantPlant.garden.domain.dto.GardenRequestDTO;
+import umc.wantPlant.garden.domain.dto.GardenResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/gardens")
 public class GardenController {
+
+	private final GardenCommandService gardenCommandService;
+
+	@PostMapping("/")
+	@Operation(
+		summary = "정원 생성 API",
+		description = "정원을 생성하는 API입니다.")
+	@ApiResponses({
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "COMMON200",
+			description = "OK, 성공"),
+	})
+	public ApiResponse<GardenResponseDTO.GardenCreatResultDTO> creat(
+		@RequestBody GardenRequestDTO.GardenCreatDTO request) {
+
+		Garden garden = gardenCommandService.creatGarden(request);
+
+		return ApiResponse.onSuccess(
+			GardenResponseDTO.GardenCreatResultDTO
+				.builder()
+				.gardenId(garden.getId())
+				.build()
+		);
+	}
+
 }
