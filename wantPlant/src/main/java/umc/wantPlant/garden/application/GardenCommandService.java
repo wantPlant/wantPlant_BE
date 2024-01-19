f5e8e3acf8c4c7f6e8ebe7cd6f73a1ea503325a9
이승준 <sengjun0624@naver.com>
2024-01-19 22:37:56 +0900

    :sparkles: FEAT : ADD creatGarden function to GardenCommandService

--- wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandService.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandService.java
@@ -1,4 +1,8 @@
 package umc.wantPlant.garden.application;
 
+import umc.wantPlant.garden.domain.Garden;
+import umc.wantPlant.garden.domain.dto.GardenRequestDTO;
+
 public interface GardenCommandService {
+	Garden creatGarden(GardenRequestDTO.CreatDTO creat);
 }
