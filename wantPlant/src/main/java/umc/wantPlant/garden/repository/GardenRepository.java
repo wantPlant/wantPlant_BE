d361f4af78453946503c04c40aedc86d753d0680
sengjun0624 <sengjun0624@naver.com>
2024-01-22 00:22:22 +0900

    :sparkles: FEAT: Add Get Garden with paging method

--- wantPlant/src/main/java/umc/wantPlant/garden/repository/GardenRepository.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/repository/GardenRepository.java
@@ -1,8 +1,14 @@
 package umc.wantPlant.garden.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import umc.wantPlant.garden.domain.Garden;
+import umc.wantPlant.garden.domain.enums.GardenCategories;
 
 public interface GardenRepository extends JpaRepository<Garden, Long> {
+	Page<Garden> findAllGardensBy(PageRequest pageRequest);
+
+	Page<Garden> findByCategory(GardenCategories category, PageRequest pageRequest);
 }
