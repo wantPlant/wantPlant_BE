c48359e502385f095805b1299e3f1269678d48fb
sengjun0624 <sengjun0624@naver.com>
2024-01-22 00:21:20 +0900

    :sparkles: FEAT: Add gardenCategory in GardenCreatDTO

--- wantPlant/src/main/java/umc/wantPlant/garden/domain/dto/GardenResponseDTO.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/domain/dto/GardenResponseDTO.java
@@ -1,5 +1,7 @@
 package umc.wantPlant.garden.domain.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,5 +14,37 @@ public class GardenResponseDTO {
 	@AllArgsConstructor
 	public static class GardenResultDTO {
 		Long gardenId;
+
+		String gardenCategory;
 	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class GardenDTO {
+		Long gardenId;
+		String name;
+		String description;
+		String gardenCategory;
+	}
+
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Getter
+	public static class GardenListDTO {
+		List<GardenDTO> gardenList;
+
+		Integer listSize;
+
+		Integer totalPage;
+
+		Long totalElements;
+
+		Boolean isFirst;
+
+		Boolean isLast;
+	}
+
 }
