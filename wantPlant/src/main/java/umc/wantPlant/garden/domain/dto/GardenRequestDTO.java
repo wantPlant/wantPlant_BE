ca0e07f2345f288d038a5f6d8697bf61890a067e
sengjun0624 <sengjun0624@naver.com>
2024-01-22 00:22:46 +0900

    :sparkles: FEAT: Add DTO for Get Method

--- wantPlant/src/main/java/umc/wantPlant/garden/domain/dto/GardenRequestDTO.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/domain/dto/GardenRequestDTO.java
@@ -1,7 +1,6 @@
 package umc.wantPlant.garden.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class GardenRequestDTO {
@@ -16,8 +15,8 @@ public class GardenRequestDTO {
 		private String description;
 
 		// TODO: String으로 정원카테고리 바꾸기
-		@NotNull
-		private Integer gardenCategory;
+		@NotBlank
+		private String category;
 	}
 
 	@Getter
