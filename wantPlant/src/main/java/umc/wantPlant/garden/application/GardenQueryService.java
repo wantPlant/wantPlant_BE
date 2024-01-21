e532239a318b95f4bc724b7c3684a10ea09c3db9
sengjun0624 <sengjun0624@naver.com>
2024-01-22 00:20:44 +0900

    :sparkles: Add and Implement QueryService Method

--- wantPlant/src/main/java/umc/wantPlant/garden/application/GardenQueryService.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/application/GardenQueryService.java
@@ -2,9 +2,15 @@ package umc.wantPlant.garden.application;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+
 import umc.wantPlant.garden.domain.Garden;
 
 public interface GardenQueryService {
-	Optional<Garden> findById(Long gardenId);
+	Optional<Garden> getGardenById(Long gardenId);
+
+	Page<Garden> getGardens(Integer page, Integer pageSize);
+
+	Page<Garden> getGardensByCategory(String category, Integer page, Integer pageSize);
 }
 
