f4d7deac034709fbe918d613919cbf25ffaa86ae
yoonho <nam2660433@gmail.com>
2024-01-23 14:23:55 +0900

    FEAT: Add Member domain

---
+++ wantPlant/src/main/java/umc/wantPlant/login/domain/Member.java
@@ -0,0 +1,22 @@
+package umc.wantPlant.login.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+}
