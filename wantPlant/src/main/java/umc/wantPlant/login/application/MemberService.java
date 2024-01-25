b9f4d0a2b39007ec816ae41efe0befe05b63c1b0
yoonho <nam2660433@gmail.com>
2024-01-25 19:46:16 +0900

    FEAT: Add findById function

--- wantPlant/src/main/java/umc/wantPlant/login/application/MemberService.java
+++ wantPlant/src/main/java/umc/wantPlant/login/application/MemberService.java
@@ -1,4 +1,21 @@
 package umc.wantPlant.login.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.wantPlant.login.domain.Member;
+import umc.wantPlant.login.repositiory.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member findById(Long memberId){
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
 }
