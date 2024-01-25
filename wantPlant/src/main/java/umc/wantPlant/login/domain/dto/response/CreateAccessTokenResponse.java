6b4fd36db05df72eee59548362e7d7ef16a249aa
yoonho <nam2660433@gmail.com>
2024-01-25 19:42:53 +0900

    FEAT: Add CreateAccessTokenResponse

---
+++ wantPlant/src/main/java/umc/wantPlant/login/domain/dto/response/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package umc.wantPlant.login.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
