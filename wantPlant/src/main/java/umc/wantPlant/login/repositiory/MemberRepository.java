f92477de80812b20995af18a0fdce5103ed073bf
yoonho <nam2660433@gmail.com>
2024-01-25 19:46:36 +0900

    FEAT: Add findByNickname function

--- wantPlant/src/main/java/umc/wantPlant/login/repositiory/MemberRepository.java
+++ wantPlant/src/main/java/umc/wantPlant/login/repositiory/MemberRepository.java
@@ -3,6 +3,8 @@ package umc.wantPlant.login.repositiory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.wantPlant.login.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByNickname(String nickname);
 }
