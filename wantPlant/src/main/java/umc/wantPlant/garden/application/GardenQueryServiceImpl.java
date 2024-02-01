5e7456564ec2829150e738bd13d5572330590856
sengjun0624 <sengjun0624@naver.com>
2024-02-01 17:45:50 +0900

    :sparkles: FEAT: Add Garden Validation Service

--- wantPlant/src/main/java/umc/wantPlant/garden/application/GardenQueryServiceImpl.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/application/GardenQueryServiceImpl.java
@@ -1,5 +1,6 @@
 package umc.wantPlant.garden.application;
 
+
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import umc.wantPlant.apipayload.code.status.ErrorStatus;
+import umc.wantPlant.apipayload.exceptions.handler.GardenCategoryHandler;
+import umc.wantPlant.apipayload.exceptions.handler.GardenHandler;
 import umc.wantPlant.garden.domain.Garden;
 import umc.wantPlant.garden.domain.enums.GardenCategories;
 import umc.wantPlant.garden.repository.GardenRepository;
@@ -20,8 +24,8 @@ public class GardenQueryServiceImpl implements GardenQueryService {
 	private final GardenRepository gardenRepository;
 
 	@Override
-	public Optional<Garden> getGardenById(Long gardenId) {
-		return gardenRepository.findById(gardenId);
+	public Garden getGardenById(Long gardenId) {
+		return gardenRepository.findById(gardenId).orElseThrow(() -> new GardenHandler(ErrorStatus.GARDEN_NOT_FOUND));
 	}
 
 	@Override
@@ -43,7 +47,25 @@ public class GardenQueryServiceImpl implements GardenQueryService {
 			case "HOBBY":
 				gardenCategory = GardenCategories.HOBBY;
 				break;
+
 		}
 		return gardenRepository.findByCategory(gardenCategory, PageRequest.of(page, pageSize));
+	}
+
+	@Override
+	public Long getGardenSize() {
+		Long count = gardenRepository.count();
+		if (count == 0)
+			throw new GardenHandler(ErrorStatus.GARDEN_NOT_EXIST);
+		return count;
+	}
+
+	@Override
+	public GardenCategories getGardenCategory(String category) {
+		try {
+			return GardenCategories.valueOf(category.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			throw new GardenCategoryHandler(ErrorStatus.GARDEN_CATEGORY_NOT_FOUND);
+		}
 	}
 }
