95294acd5bfab4cb22edb5fcfb8588d97da2e271
yoonho <nam2660433@gmail.com>
2024-02-02 02:55:58 +0900

    FEAT: Add RefreshToken

---
+++ wantPlant/src/main/java/umc/wantPlant/member/domain/RefreshToken.java
@@ -0,0 +1,21 @@
+package umc.wantPlant.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+}
