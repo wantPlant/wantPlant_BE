761c32b39521b266bb1baf95855caf0b8a1c7ebf
yoonho <nam2660433@gmail.com>
2024-02-02 02:55:37 +0900

    FEAT: Add MemberRepository

---
+++ wantPlant/src/main/java/umc/wantPlant/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package umc.wantPlant.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.wantPlant.member.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
