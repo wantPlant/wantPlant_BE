c92c3b74c26621a84c9a1cf40ab5ea8af5c433e7
yoonho <nam2660433@gmail.com>
2024-02-03 11:53:39 +0900

    FEAT: SwaggerConfig 수정

--- wantPlant/src/main/java/umc/wantPlant/config/SwaggerConfig.java
+++ wantPlant/src/main/java/umc/wantPlant/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 				.description("WantPlan API 명세서")
 				.version("1.0.0");
 
-		String jwtSchemeName = "JWT TOKEN";
+		String jwtSchemeName = "Authorization";
 
 		// API 요청헤더에 인증정보 포함
 		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
