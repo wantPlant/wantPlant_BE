f855e81b0e420f5a2b48909d2b98cec0c67dfa44
yoonho <nam2660433@gmail.com>
2024-02-07 17:21:11 +0900

    REFACTOR: 로직에 맞게 파라미터 수정

--- wantPlant/src/main/java/umc/wantPlant/member/application/MemberService.java
+++ wantPlant/src/main/java/umc/wantPlant/member/application/MemberService.java
@@ -3,7 +3,6 @@ package umc.wantPlant.member.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import umc.wantPlant.config.security.jwt.JwtTokenProvider;
 import umc.wantPlant.member.domain.Member;
 import umc.wantPlant.member.domain.dto.response.MemberInfoResponseDTO;
 import umc.wantPlant.member.repository.MemberRepository;
@@ -12,11 +11,9 @@ import umc.wantPlant.member.repository.MemberRepository;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
-    public MemberInfoResponseDTO getMemberInfo(final String accessToken){
-        Long id = Long.valueOf(jwtTokenProvider.getMemberIdByToken(accessToken));
+    public MemberInfoResponseDTO getMemberInfo(Long id){
         Member member = memberRepository.findById(id).orElseThrow(() -> new RuntimeException());
 
         return MemberInfoResponseDTO.builder()
