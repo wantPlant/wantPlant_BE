c57f06937d64e03f0d8fbe3cda6b86f093394e94
yoonho <nam2660433@gmail.com>
2024-02-07 17:43:24 +0900

    REFACTOR: 로직에 맞게 파라미터 수정, operation 추가

--- wantPlant/src/main/java/umc/wantPlant/member/ui/MemberController.java
+++ wantPlant/src/main/java/umc/wantPlant/member/ui/MemberController.java
@@ -1,10 +1,8 @@
 package umc.wantPlant.member.ui;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.wantPlant.apipayload.ApiResponse;
 import umc.wantPlant.member.application.MemberService;
 import umc.wantPlant.member.domain.dto.response.MemberInfoResponseDTO;
@@ -14,8 +12,11 @@ import umc.wantPlant.member.domain.dto.response.MemberInfoResponseDTO;
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
-    @GetMapping
-    public ApiResponse<MemberInfoResponseDTO> getMemberInfo(@RequestHeader(value = "Authorization")String accessToken){
-        return ApiResponse.onSuccess(memberService.getMemberInfo(accessToken));
+
+
+    @GetMapping("/{memberId}")
+    @Operation(summary = "유저 정보 조회 API", description = "id를 통해 유저 정보를 조회하는 API")
+    public ApiResponse<MemberInfoResponseDTO> getMemberInfo(@PathVariable Long memberId){
+        return ApiResponse.onSuccess(memberService.getMemberInfo(memberId));
     }
 }
