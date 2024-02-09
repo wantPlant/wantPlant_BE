d12696baf0e9561b5f148ad9eae60c362164dfd1
sengjun0624 <sengjun0624@naver.com>
2024-02-09 22:48:32 +0900

    :recycle: REFACTOR: 정원 Command 서비스  이름 수정, 파라미터 변경

--- wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandService.java
+++ wantPlant/src/main/java/umc/wantPlant/garden/application/GardenCommandService.java
@@ -5,20 +5,13 @@ import org.springframework.data.domain.Page;
 import umc.wantPlant.garden.domain.Garden;
 import umc.wantPlant.garden.domain.dto.GardenRequestDTO;
 import umc.wantPlant.garden.domain.dto.GardenResponseDTO;
+import umc.wantPlant.member.domain.Member;
 
 public interface GardenCommandService {
-	GardenResponseDTO.GardenCreatResultDTO creat(GardenRequestDTO.GardenCreatDTO creat);
-
-	GardenResponseDTO.GardenUpdateResultDTO update(GardenRequestDTO.UpdateGardenDTO update);
-
-	GardenResponseDTO.GardenUpdateResultDTO updateName(GardenRequestDTO.UpdateGardenDTO update);
-
-	GardenResponseDTO.GardenUpdateResultDTO updateDescription(GardenRequestDTO.UpdateGardenDTO update);
-
-	GardenResponseDTO.GardenResultList getGardenList(Long memberId);
-	GardenResponseDTO.GardenListDTO getGardenList(Page<Garden> gardenPage);
-
-	GardenResponseDTO.GardenResultDTO toGardenResultDTO(Garden garden);
-
-	void delete(Long memberId, Long gardenId);
+	GardenResponseDTO.GardenCreatResultDTO toCreatGarden(Member member,GardenRequestDTO.GardenCreatRequestDTO toCreatGarden);
+	GardenResponseDTO.GardenGetResultPageDTO toGetGardenByPage(Page<Garden> GardenPageRequestDTO);
+	GardenResponseDTO.GardenGetResultListDTO toGetGardenByList(Member member);
+	GardenResponseDTO.GardenUpdateResultDTO toUpdateGarden(Member member ,GardenRequestDTO.GardenUpdateRequestDTO update);
+	GardenResponseDTO.GardenGetResultDTO toGardenResultDTO(Garden garden);
+	void toDeleteGarden(Long memberId, Long gardenId);
 }
