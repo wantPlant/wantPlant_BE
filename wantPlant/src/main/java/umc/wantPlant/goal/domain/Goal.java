c5e5a0577bd792550a3fecbe87df1736a6ddb98a
sengjun0624 <sengjun0624@naver.com>
2024-02-17 19:01:28 +0900

    :bug: HOTFIX: 정원 삭제 에러해결

--- wantPlant/src/main/java/umc/wantPlant/goal/domain/Goal.java
+++ wantPlant/src/main/java/umc/wantPlant/goal/domain/Goal.java
@@ -1,7 +1,21 @@
 package umc.wantPlant.goal.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import umc.wantPlant.pot.domain.Pot;
 
 @Entity
@@ -10,18 +24,19 @@ import umc.wantPlant.pot.domain.Pot;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Goal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long goalId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long goalId;
 
-    @Column(nullable= false, length = 40)
-    private String goalTitle;
+	@Column(nullable = false, length = 40)
+	private String goalTitle;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "pot_id")
-    private Pot pot;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "pot_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Pot pot;
 
-    public void setGoalTitle(String goalTitle){
-        this.goalTitle =goalTitle;
-    }
+	public void setGoalTitle(String goalTitle) {
+		this.goalTitle = goalTitle;
+	}
 }
